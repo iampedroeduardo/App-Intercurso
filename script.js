d7f45a31c2c2dea75ee9ee0095ef7a8ebeef1a1b
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2023-09-28 17:25:31 +0000

    inicio

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-
+class Time{
+    nome;
+    curso;
+    pontos;//esse atributo funciona para pontuação geral quando time está dentro do esporte e pontuação de jogo no jogo
+    constructor(nome,curso){
+        this.nome=nome;
+        this.curso=curso;
+    }
+}
+class Esporte{
+    nome;
+    times;
+    jogos;
+    constructor(nome){
+        this.nome=nome;
+        this.times=[];
+        this.jogos=[];
+    }
+    adicionaTime(time){
+        this.times.push(time);
+    }
+    geraJogos(){
+        for(let i=0;i<this.times.length-2;i++){
+            for(let j=0;j<this.times.length-1;j++){
+                let jogo= new Jogo(this.times[i],this.times[j]);
+                this.jogos.push(jogo);
+            }
+        }
+    };
+}
+class Jogo{
+    times;
+    constructor(time1,time2){
+        this.times.push(time1);
+        this.times.push(time2);
+        this.times[0].pontos=0;
+        this.times[1].pontos=0;
+    }
+    aumentaPontos(time){//time é um número 0 ou 1 informado no onclick do aumento de pontuação
+        this.times[time].pontos++;
+    }
+}
+let volei= new Esporte("Volei");
+let futsal= new Esporte("Futsal");
+let basquete= new Esporte("Basquete");
