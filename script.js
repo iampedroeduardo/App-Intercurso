74ef61d4ccf63be0acb5ae4f33a8759433cf5a0b
Minikito <aocosta06@gmail.com>
2023-10-04 21:39:52 -0300

    Jogos

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ class Time{
     curso;
     pontos;//esse atributo funciona para pontuação geral quando time está dentro do esporte e pontuação de jogo no jogo
     avatar;
-    constructor(nome,curso){
+    constructor(nome,curso) {
         this.nome=nome;
         this.curso=curso;
         this.pontos=0;
@@ -22,10 +22,12 @@ class Esporte{
     adicionaTime(time){
         this.times.push(time);
     }
-    geraJogos(){
-        for(let i=0;i<this.times.length-2;i++){
-            for(let j=0;j<this.times.length-1;j++){
+    geraJogos() {
+        for(let i=0; i<this.times.length-1; i++) {
+            for(let j=i+1; j<this.times.length; j++) {
+                console.log(this.times[i],this.times[j]);
                 let jogo= new Jogo(this.times[i],this.times[j]);
+                console.log(jogo);
                 this.jogos.push(jogo);
             }
         }
@@ -35,6 +37,7 @@ class Esporte{
 class Jogo{
     times;
     constructor(time1,time2){
+        this.times = [];
         this.times.push(time1);
         this.times.push(time2);
     }
@@ -72,8 +75,150 @@ function adiciona(){
         document.querySelector("#volei").checked=false;
         document.querySelector("#basquete").checked=false;
     }
+
+    else {
+        alert("Preencha todos os dados")
+    }
+}
+
+function comeca(){
+    if (volei.times.length == 1 ) {
+        alert("No mínimo dois times");
+    }
+
+    if (futsal.times.length == 1 ) {
+        alert("No mínimo dois times");
+    }
+
+    if (basquete.times.length == 1 ) {
+        alert("No mínimo dois times");
+    }
+
+    if (volei.times.length != 1 && futsal.times.length != 1 && basquete.times.length != 1 ) {
+        volei.geraJogos();
+        futsal.geraJogos();
+        basquete.geraJogos();
+        document.getElementById("inicio").style = "display: none;";
+        document.getElementById("jogos").style = "";
+
+        // Cria Jogos
+
+        if (basquete.times.length != 0) {
+            let conteudo = document.getElementById("basquete-div");
+
+            for (let i=0; i < basquete.jogos.length; i++) {
+                let div = document.createElement("div");
+                let time_nome_1 = basquete.jogos[i].times[0].nome;
+                let time_nome_2 = basquete.jogos[i].times[1].nome;
+
+                let n1 = document.createElement("h3");
+                let n2 = document.createElement("h3");
+
+                div.setAttribute("id", "jogo-basquete-" + (i+1))
+
+                n1.innerHTML = time_nome_1;
+                n2.innerHTML = time_nome_2;
+                div.appendChild(n1);
+                div.appendChild(n2);
+                conteudo.appendChild(div);
+            }
+        }
+
+        else {
+            let conteudo = document.getElementById("futsal-div");
+
+            // Aqui dentro colocar "Não houve jogos desta modalidade"
+            conteudo.innerHTML = "<h2> Não houve jogos desta modalidade </h2>"
+        }
+
+        if (futsal.times.length != 0) {
+            let conteudo = document.getElementById("futsal-div");
+
+            for (let i=0; i < futsal.jogos.length; i++) {
+                let div = document.createElement("div");
+                let time_nome_1 = futsal.jogos[i].times[0].nome;
+                let time_nome_2 = futsal.jogos[i].times[1].nome;
+
+                let n1 = document.createElement("h3");
+                let n2 = document.createElement("h3");
+
+                div.setAttribute("id", "jogo-futsal-" + (i+1))
+
+                n1.innerHTML = time_nome_1;
+                n2.innerHTML = time_nome_2;
+                div.appendChild(n1);
+                div.appendChild(n2);
+                conteudo.appendChild(div);
+            }
+        }
+
+        else {
+            let conteudo = document.getElementById("futsal-div");
+
+            // Aqui dentro colocar "Não houve jogos desta modalidade"
+            conteudo.innerHTML = "<h2> Não houve jogos desta modalidade </h2>"
+        }
+
+        if (volei.times.length != 0) {
+            let conteudo = document.getElementById("volei-div");
+
+            for (let i=0; i < volei.jogos.length; i++) {
+                let div = document.createElement("div");
+                let time_nome_1 = volei.jogos[i].times[0].nome;
+                let time_nome_2 = volei.jogos[i].times[1].nome;
+
+                let n1 = document.createElement("h3");
+                let n2 = document.createElement("h3");
+
+                div.setAttribute("id", "jogo-volei-" + (i+1))
+
+                n1.innerHTML = time_nome_1;
+                n2.innerHTML = time_nome_2;
+                div.appendChild(n1);
+                div.appendChild(n2);
+                conteudo.appendChild(div);
+            }
+        }
+
+        else {
+            let conteudo = document.getElementById("volei-div");
+
+            // Aqui dentro colocar "Não houve jogos desta modalidade"
+            conteudo.innerHTML = "<h2> Não houve jogos desta modalidade </h2>"
+        }
+    }
+}
+
+// Funções das Modalidades
+
+function Basquete() {
+    document.getElementById("basquete-div").style = "";
+    document.getElementById("futsal-div").style = "display: none;";
+    document.getElementById("volei-div").style = "display: none;";
+}
+
+function Futsal() {
+    document.getElementById("futsal-div").style = "";
+    document.getElementById("basquete-div").style = "display: none;";
+    document.getElementById("volei-div").style = "display: none;";
+}
+
+function Volei() {
+    document.getElementById("volei-div").style = "";
+    document.getElementById("basquete-div").style = "display: none;";
+    document.getElementById("futsal-div").style = "display: none;";
 }
 
 let volei= new Esporte("Volei");
 let futsal= new Esporte("Futsal");
 let basquete= new Esporte("Basquete");
+
+//Teste
+let team1 = new Time("time 1", "Fabricação Mecânica");
+let team2 = new Time("time 2", "Informática para a Internet");
+
+basquete.adicionaTime(team1);
+basquete.adicionaTime(team2);
+// Fim Teste
+
+// LOCAL STORAGE
